e1f28bd10e480a36f2a2d32ed0cb8d2807ee40f0
Hans <zhikangtsang@163.com>
2025-06-27 21:18:55 +0800

    [vote] feat: init vote module
    1.仅初始化了vote模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { VoterModule } from './voter/voter.module';
 import { VoteMgmtModule } from './vote-mgmt/vote-mgmt.module';
 import { SquareModule } from './square/square.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/services/auth.service';
 import { AuthController } from './auth/controllers/auth.controller';
+import { VoteModule } from './vote/vote.module';
 
 
 @Module({
-  imports: [VoteMgmtModule, VoterModule, SquareModule, AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, VoteMgmtModule, VoteModule, SquareModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService],
 })
