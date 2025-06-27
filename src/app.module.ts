7bbe6e73a4e722dc83897d0e82004c049ea18c17
Hans <zhikangtsang@163.com>
2025-06-27 16:47:03 +0800

    feat: init module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VoterModule } from './voter/voter.module';
+import { VoteMgmtModule } from './vote-mgmt/vote-mgmt.module';
+import { SquareModule } from './square/square.module';
 
 @Module({
-  imports: [],
+  imports: [VoterModule, VoteMgmtModule, SquareModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
