75d10e20e5e727d4bf555979eab87576bc89d944
Hans <zhikangtsang@163.com>
2025-06-28 13:14:45 +0800

    [square] feat: init square module

--- src/square/square.module.ts
+++ src/square/square.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SquareController } from './controllers/square.controller';
+import { SquareService } from './services/square.service';
 
-@Module({})
+@Module({
+    controllers: [SquareController],
+    providers: [SquareService],
+})
 export class SquareModule {}
